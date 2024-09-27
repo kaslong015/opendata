8348e899d9cd64f568c2c88ee37be26cb7e9ed81
kaslong <kevinkaslong@gmail.com>
2024-09-27 08:59:55 +0100

    rename requirements.txt

--- core/views.py
+++ core/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from .models import *
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
-
-
 def dashboard(request):
     results = Licenses.objects.all().order_by('id')
-    
-
     context = {        
         'licenses':results,
         'total_licenses':results.count(),
     }
     return render(request,'core/dashboard.html',context)
+
+
+def appliction_view():
+    pass
