9ef16097495d42af98decf1c6baf270631afe336
kaslong <kevinkaslong@gmail.com>
2024-09-30 10:09:50 +0100

    counts updated

--- core/views.py
+++ core/views.py
@@ -4,20 +4,30 @@ from .models import *
 # Create your views here.
 def dashboard(request):
     results = Licenses.objects.all().order_by('id')
+    blocked = blockedLicenses.objects.all().count()
+    registed = registeredApplications.objects.all().count()
+    pending = pendingApplications.objects.all().count()
     context = {        
         'licenses':results,
         'total_licenses':results.count(),
+        'blocked':blocked,
+        'registed':registed,
+        'pending':pending
     }
     return render(request,'core/dashboard.html',context)
 
 
 def blocked_licenses_view(request):
     results = blockedLicenses.objects.all()
-    results1 = Licenses.objects.all().order_by('id')
+    results1 = Licenses.objects.all().order_by('id')   
+    registed = registeredApplications.objects.all().count()
+    pending = pendingApplications.objects.all().count()
     context = {        
         'licenses':results,
         'total_licenses':results1.count(),
-        'total_blocked_licenses':results.count(),
+        'blocked':results.count(),        
+        'registed':registed,
+        'pending':pending
     }
     return render(request,'core/block.html',context)
 
@@ -26,10 +36,14 @@ def registed_measures_view(request):
     results1 = Licenses.objects.all().order_by('id')
     measures = registered_measures.objects.all()
     blocked_licenses = blockedLicenses.objects.all()
+    registed = registeredApplications.objects.all().count()
+    pending = pendingApplications.objects.all().count()
     context = {        
         'measures':measures,
         'total_licenses':results1.count(),
-        'total_blocked_licenses':blocked_licenses.count(),
+        'blocked':blocked_licenses.count(),                
+        'registed':registed,
+        'pending':pending
     }
     return render(request,'core/registed_measure.html',context)
 
@@ -38,10 +52,14 @@ def last_month_view(request):
     results = blockedLicenses.objects.all()
     results1 = Licenses.objects.all().order_by('id')
     lastmonthLicenses = lastMonthLicenses.objects.all()
+    registed = registeredApplications.objects.all().count()
+    pending = pendingApplications.objects.all().count()
     context = {        
         'licenses':lastmonthLicenses,
         'total_licenses':results1.count(),
-        'total_blocked_licenses':results.count(),
+        'blocked':results.count(),
+        'registed':registed,
+        'pending':pending
     }
     return render(request,'core/lastmonth.html',context)
 
@@ -51,10 +69,14 @@ def pending_measures_view(request):
     results = blockedLicenses.objects.all()
     results1 = Licenses.objects.all().order_by('id')
     lastmonthLicenses = pendingMeasures.objects.all()
+    registed = registeredApplications.objects.all().count()
+    pending = pendingApplications.objects.all().count()
     context = {        
         'licenses':lastmonthLicenses,
         'total_licenses':results1.count(),
-        'total_blocked_licenses':results.count(),
+        'blocked':results.count(),
+        'registed':registed,
+        'pending':pending
     }
     return render(request,'core/pending_measure.html',context)
 
@@ -64,10 +86,13 @@ def pending_application_view(request):
     results = blockedLicenses.objects.all()
     results1 = Licenses.objects.all().order_by('id')
     lastmonthLicenses = pendingApplications.objects.all()
+    registed = registeredApplications.objects.all().count()
+    pending = pendingApplications.objects.all().count()
     context = {        
         'licenses':lastmonthLicenses,
         'total_licenses':results1.count(),
-        'total_blocked_licenses':results.count(),
+        'blocked':results.count(),
+        'recent':lastmonthLicenses.count()
     }
     return render(request,'core/pen-app.html',context)
 
@@ -81,5 +106,6 @@ def register_application_view(request):
         'licenses':lastmonthLicenses,
         'total_licenses':results1.count(),
         'total_blocked_licenses':results.count(),
+        'recent':lastmonthLicenses.count()
     }
     return render(request,'core/reg-app.html',context)
